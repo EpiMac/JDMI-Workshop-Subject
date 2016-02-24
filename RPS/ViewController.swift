a81f6dd0814c1e5cab6cae28802d8649bb282a12
Alexandre Abita <abita_a@epita.fr>
2016-02-24 01:47:41 +0100

    remove the correction

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -1,13 +1,13 @@
 //
 //  ViewController.swift
 //  RPS
-//  Copyright © 2016 EpiMac. All rights reserved.
+//  Copyright © 2016 Alexandre Toubiana. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController
+{
     // Variables globales.
     var playerChoice = -1
     var computerChoice = -1
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     // Choix de l'adversaire
     @IBOutlet weak var computerChoiceImage: UIImageView!
 
+    
     // Fonction appliquée au bouton "recommencer"
     @IBAction func recommencerButton(sender: AnyObject)
     {
@@ -37,16 +38,11 @@ class ViewController: UIViewController {
     // Fonction qui restaure les variables par default
     func setup()
     {
-        playerChoice = -1
-        computerChoice = -1
-        computerChoiceImage.image = nil
-        resultLabel.text = "VS"
-        scorePlayer = 0
-        scoreComputer = 0
-        scoreComputerLabel.text = "0"
-        scorePlayerLabel.text = "0"
+        /* ----------- */
+        /* FIXME Setup */
+        /* ----------- */
     }
-    
+
     // Fonction qui choisit un nombre aléatoire qui peut être 0, 1 ou 2.
     // Cette fonction permet donc de simuler le choix de l'adversaire.
     func computer()->Int
@@ -58,59 +54,33 @@ class ViewController: UIViewController {
     // Cette fonction permet de décider l'action du joueur.
     @IBAction func makeChoice(sender: AnyObject)
     {
-        playerChoice = 1;
-        computerChoice = computer()
-        finality(playerChoice, CC: computerChoice)
+        /* ---------------- */
+        /* FIXME MakeChoice */
+        /* ---------------- */
     }
     
     // Fonction qui définit le résultat en fonction
     // du choix de l'ordinateur et du joueur.
     func finality(PC: Int, CC: Int)
     {
-        switch CC
-        {
-        case 0:
-            computerChoiceImage.image = UIImage(named: "Rock")
-        case 1:
-            computerChoiceImage.image = UIImage(named: "Paper")
-        case 2:
-            computerChoiceImage.image = UIImage(named: "Scissors")
-        default:
-            computerChoiceImage.image = nil
-        }
-        if (PC == CC)
-        {
-            resultLabel.text = "Égalité"
-        }
-        else if ((PC == 0 && CC == 2)||(PC == 1 && CC == 0)||(PC == 2 && CC == 1))
-        {
-            resultLabel.text = "Gagné"
-            scorePlayerLabel.text = "\(scorePlayer++)"
-            
-        }
-        else
-        {
-            resultLabel.text = "Perdu"
-            scoreComputerLabel.text = "\(scoreComputer++)"
-        }
+        /* -------------- */
+        /* FIXME finality */
+        /* -------------- */
     }
     
-    /* ============================================== */
-    /*               DO NOT TOUCH IT !                */
-    /* ============================================== */
+/* ============================================== */
+/*               DO NOT TOUCH IT !                */
+/* ============================================== */
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setup()
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
--- RPSTests/RPSTests.swift
+++ RPSTests/RPSTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import RPS
 
 class RPSTests: XCTestCase {
     
