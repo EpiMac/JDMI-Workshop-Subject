ea9ce78a6e601e5698b5c150b44a608524970ffe
Alexandre Abita <abita_a@epita.fr>
2016-02-24 01:39:19 +0100

    Fix the project for Xcode 6.1.1

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -1,13 +1,13 @@
 //
 //  ViewController.swift
 //  RPS
-//  Copyright © 2016 Alexandre Toubiana. All rights reserved.
+//  Copyright © 2016 EpiMac. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController
-{
+class ViewController: UIViewController {
+    
     // Variables globales.
     var playerChoice = -1
     var computerChoice = -1
@@ -27,7 +27,6 @@ class ViewController: UIViewController
     // Choix de l'adversaire
     @IBOutlet weak var computerChoiceImage: UIImageView!
 
-    
     // Fonction appliquée au bouton "recommencer"
     @IBAction func recommencerButton(sender: AnyObject)
     {
@@ -38,11 +37,16 @@ class ViewController: UIViewController
     // Fonction qui restaure les variables par default
     func setup()
     {
-        /* ----------- */
-        /* FIXME Setup */
-        /* ----------- */
+        playerChoice = -1
+        computerChoice = -1
+        computerChoiceImage.image = nil
+        resultLabel.text = "VS"
+        scorePlayer = 0
+        scoreComputer = 0
+        scoreComputerLabel.text = "0"
+        scorePlayerLabel.text = "0"
     }
-
+    
     // Fonction qui choisit un nombre aléatoire qui peut être 0, 1 ou 2.
     // Cette fonction permet donc de simuler le choix de l'adversaire.
     func computer()->Int
@@ -54,33 +58,59 @@ class ViewController: UIViewController
     // Cette fonction permet de décider l'action du joueur.
     @IBAction func makeChoice(sender: AnyObject)
     {
-        /* ---------------- */
-        /* FIXME MakeChoice */
-        /* ---------------- */
+        playerChoice = 1;
+        computerChoice = computer()
+        finality(playerChoice, CC: computerChoice)
     }
     
     // Fonction qui définit le résultat en fonction
     // du choix de l'ordinateur et du joueur.
     func finality(PC: Int, CC: Int)
     {
-        /* -------------- */
-        /* FIXME finality */
-        /* -------------- */
+        switch CC
+        {
+        case 0:
+            computerChoiceImage.image = UIImage(named: "Rock")
+        case 1:
+            computerChoiceImage.image = UIImage(named: "Paper")
+        case 2:
+            computerChoiceImage.image = UIImage(named: "Scissors")
+        default:
+            computerChoiceImage.image = nil
+        }
+        if (PC == CC)
+        {
+            resultLabel.text = "Égalité"
+        }
+        else if ((PC == 0 && CC == 2)||(PC == 1 && CC == 0)||(PC == 2 && CC == 1))
+        {
+            resultLabel.text = "Gagné"
+            scorePlayerLabel.text = "\(scorePlayer++)"
+            
+        }
+        else
+        {
+            resultLabel.text = "Perdu"
+            scoreComputerLabel.text = "\(scoreComputer++)"
+        }
     }
     
-/* ============================================== */
-/*               DO NOT TOUCH IT !                */
-/* ============================================== */
+    /* ============================================== */
+    /*               DO NOT TOUCH IT !                */
+    /* ============================================== */
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setup()
+
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
 
