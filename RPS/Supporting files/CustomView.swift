d63f4e6474c2ade44ba112510ee8a8ac7c179f11
Alexandre Toubiana <alextoub@hotmail.com>
2018-01-04 23:36:37 +0100

    Change design

---
+++ RPS/Supporting files/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  RPS
+//
+//  Created by Alexandre Toubiana on 04/01/2018.
+//  Copyright © 2018 Alexandre Toubiana. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ RPS/Supporting files/ViewControllerExt.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerExt.swift
+//  RPS
+//
+//  Created by Alexandre Toubiana on 04/01/2018.
+//  Copyright © 2018 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
